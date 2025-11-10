66a73af1644aae8e4e0dc8b72ba26f8c5420c503
cjwils <cjwilson075@gmail.com>
2025-11-10 12:06:17 +1100

    edit main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
+import { MongooseModule } from '@nestjs/mongoose'; 
+import { TasksModule } from './tasks/tasks.module'; 
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Module to load environment variables
+    ConfigModule.forRoot({ 
+      isGlobal: true, 
+      envFilePath: ['.env.development.local', '.env'], 
+    }), 
+    // Module to connect to MongoDB
+    MongooseModule.forRootAsync({ 
+      inject: [ConfigService], 
+      useFactory: (config: ConfigService) => { 
+        const useAtlas = config.get<string>('USE_ATLAS') === 'true'; 
+        const uri = useAtlas 
+          ? config.get<string>('MONGODB_URI_ATLAS') 
+          : config.get<string>('MONGODB_URI'); 
+        if (!uri) { 
+          throw new Error('Missing MongoDB URI. Check your .env values.'); 
+        } 
+        return { uri }; 
+      }, 
+    }),
+    TasksModule,
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
